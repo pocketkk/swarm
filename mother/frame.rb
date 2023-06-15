8d4f7e056a1cf5731384c9d6085413bfd8ae479f
Jason Crump <pocketkk@gmail.com>
2023-06-15 06:01:02 -0700

    Refactor out some window stuff

---
+++ mother/frame.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'curses'
+
+class Frame
+  include Curses
+
+  LEFT_TOP_CORNER = '┌'
+  RIGHT_TOP_CORNER = '┐'
+  LEFT_BOTTOM_CORNER = '└'
+  RIGHT_BOTTOM_CORNER = '┘'
+  HORIZONTAL_LINE = '─'
+  VERTICAL_LINE = '│'
+
+  BORDER_SPACE = 2
+
+  attr_reader :window, :title
+
+  def initialize(window, title)
+    @window = window
+    @title = title
+  end
+
+  def framed_window
+    draw_horizontal_borders
+    draw_vertical_borders
+    window.attrset(A_NORMAL)
+    window
+  end
+
+  private
+
+  def dashes_count
+    ((window.maxx - 2 - title.length) / 2)
+  end
+
+  def offset_count
+    title.bytesize.odd? ? 1 : 0
+  end
+
+  def draw_horizontal_borders
+    window.addstr(LEFT_TOP_CORNER + HORIZONTAL_LINE * dashes_count.floor + title + HORIZONTAL_LINE * (dashes_count.ceil + offset_count) + RIGHT_TOP_CORNER)
+    window.setpos(window.maxy - 1, 0)
+    window.addstr(LEFT_BOTTOM_CORNER + HORIZONTAL_LINE * (window.maxx - BORDER_SPACE) + RIGHT_BOTTOM_CORNER)
+  end
+
+  def draw_vertical_borders
+    (1..window.maxy - BORDER_SPACE).each do |row|
+      window.setpos(row, 0)
+      window.addstr(VERTICAL_LINE)
+      window.setpos(row, window.maxx - 1)
+      window.addstr(VERTICAL_LINE)
+    end
+  end
+end
--- mother/mother.rb
+++ mother/mother.rb
@@ -4,6 +4,7 @@ require 'curses'
 require 'docker'
 require 'pry'
 require 'forwardable'
+require_relative 'frame'
 
 class Agent
   extend Forwardable
@@ -33,40 +34,25 @@ class ChatUI
   INTERNAL_PADDING = 2
   CHILD_PADDING = 1
 
-  LEFT_TOP_CORNER = '┌'
-  RIGHT_TOP_CORNER = '┐'
-  LEFT_BOTTOM_CORNER = '└'
-  RIGHT_BOTTOM_CORNER = '┘'
-  HORIZONTAL_LINE = '─'
-  VERTICAL_LINE = '│'
+  BORDER_SPACE = 2
 
   def create_chatbots_subwindow(main_window)
-    chatbots_subwindow_width = main_window.maxx - (INTERNAL_PADDING * 2)
+    chatbots_subwindow_width = main_window.maxx - (INTERNAL_PADDING * BORDER_SPACE)
+    chatbots_subwindow_height = BORDER_SPACE + (PADDING_HEIGHT + INTERNAL_PADDING + BORDER_WIDTH)
 
-    chatbots_subwindow_height = 2 + (PADDING_HEIGHT + INTERNAL_PADDING + BORDER_WIDTH)
+    chatbots_subwindow = main_window.subwin(
+      chatbots_subwindow_height,
+      chatbots_subwindow_width,
+      PADDING_HEIGHT + INTERNAL_PADDING,
+      INTERNAL_PADDING + PADDING_WIDTH
+    )
 
-    chatbots_subwindow = main_window.subwin(chatbots_subwindow_height, chatbots_subwindow_width, PADDING_HEIGHT + INTERNAL_PADDING, INTERNAL_PADDING + PADDING_WIDTH)
+    framed = Frame.new(
+      chatbots_subwindow,
+      '<<*** AGENTS ***'
+    )
 
-    chatbots_subwindow_title = '<<*** AGENTS ***>>'
-    dashes_count = (chatbots_subwindow_width - 2 - chatbots_subwindow_title.length) / 2
-
-    offset_count = 0
-    offset_count += 1 if dashes_count * 2 + chatbots_subwindow_title.size != chatbots_subwindow_width
-    chatbots_subwindow.addstr(LEFT_TOP_CORNER + HORIZONTAL_LINE * dashes_count + chatbots_subwindow_title + HORIZONTAL_LINE * (dashes_count + offset_count) + RIGHT_TOP_CORNER)
-
-    (1..chatbots_subwindow_height - 2).each do |row|
-      chatbots_subwindow.setpos(row, 0)
-      chatbots_subwindow.addstr(VERTICAL_LINE)
-      chatbots_subwindow.setpos(row, chatbots_subwindow_width - 1)
-      chatbots_subwindow.addstr(VERTICAL_LINE)
-    end
-
-    chatbots_subwindow.setpos(chatbots_subwindow_height - 1, 0)
-    chatbots_subwindow.addstr(LEFT_BOTTOM_CORNER + HORIZONTAL_LINE * (chatbots_subwindow_width - 2) + RIGHT_BOTTOM_CORNER)
-    chatbots_subwindow.setpos(chatbots_subwindow_height, chatbots_subwindow_width)
-    chatbots_subwindow.attrset(A_NORMAL)
-
-    chatbots_subwindow
+    framed.framed_window
   end
 
   def wrapper
@@ -76,41 +62,16 @@ class ChatUI
     main_window_width = cols - (BORDER_WIDTH * 2) - (PADDING_WIDTH * 2)
     main_window_height = lines - (BORDER_WIDTH * 2) - (PADDING_WIDTH * 2)
 
-    text_area_width = main_window_width - (INTERNAL_PADDING * 2)
-    text_area_height = main_window_height - (INTERNAL_PADDING * 2)
-
     main_window = Curses::Window.new(main_window_height, main_window_width, PADDING_WIDTH, PADDING_WIDTH)
     main_window.attron(color_pair(1))  # Apply color pair to the main_window
 
-    # Draw the border manually
-    main_window.setpos(0, 0)
-    main_window.attrset(A_BOLD)
-    main_window_title = ' MOTHER '
-    dashes_count = ((main_window_width - 2) - main_window_title.size) / 2
-    main_window.addstr(LEFT_TOP_CORNER + HORIZONTAL_LINE * dashes_count + main_window_title + HORIZONTAL_LINE * (dashes_count + 1) + RIGHT_TOP_CORNER)
+    framed = Frame.new(
+      main_window,
+      'The Agencys'
+    )
 
-    (1..main_window_height-2).each do |row| # note the change in the range
-      main_window.setpos(row, 0)
-      main_window.addstr(VERTICAL_LINE)
-      main_window.setpos(row, main_window_width - 1)
-      main_window.addstr(VERTICAL_LINE)
-    end
-
-    main_window.setpos(main_window_height-1, 0)
-    main_window.addstr(LEFT_BOTTOM_CORNER + HORIZONTAL_LINE * (main_window_width - 2) + HORIZONTAL_LINE + RIGHT_BOTTOM_CORNER)
-    main_window.setpos(main_window_height, main_window_width)
-    main_window.addstr(RIGHT_BOTTOM_CORNER)
-    main_window.attrset(A_NORMAL)
-    main_window
+    framed.framed_window
   end
-
-  LEFT_TOP_CORNER = "┌"
-  RIGHT_TOP_CORNER = "┐"
-  LEFT_BOTTOM_CORNER = "└"
-  RIGHT_BOTTOM_CORNER = "┘"
-  HORIZONTAL_LINE = "─"
-  VERTICAL_LINE = "│"
-
 
   def agent_message(agent, old)
     message = agent.logs(stdout: true, tail: 1).gsub("\0", '').gsub("\r\n", "").gsub(/\^A9/, '')
@@ -132,11 +93,8 @@ class ChatUI
       text_area_start_x = PADDING_WIDTH + INTERNAL_PADDING
       text_area_start_y = PADDING_HEIGHT
 
-      bot1_row = 1
-      bot2_row = 2
-
-      bot1_message = "starting ..."
-      bot2_message = "starting ..."
+      bot1_message = 'starting ...'
+      bot2_message = 'starting ...'
 
       count = 0
 
@@ -145,7 +103,6 @@ class ChatUI
 
       bot1_message_old = ''
       bot2_message_old = ''
-      logs_old = ""
 
       openai_chat_bot = \
         Agent.new(
@@ -173,7 +130,6 @@ class ChatUI
       puts agents.map(&:start)
 
       loop do
-
         agents.each do |agent|
           chatbot_window.setpos(text_area_start_y + agent.row, text_area_start_x)
           message = agent_message(agent, agent.previous_message)
