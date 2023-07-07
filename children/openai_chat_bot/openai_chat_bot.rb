f1215161294f737405c332062d0595977c4ebea3
Jason Crump <pocketkk@gmail.com>
2023-07-07 16:04:50 -0700

    it works, saving

--- children/openai_chat_bot/openai_chat_bot.rb
+++ children/openai_chat_bot/openai_chat_bot.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'redis'
 require 'logger'
 require_relative 'pg_client'
+require_relative 'service_nanny'
 
 class OpenAIChatBot
   API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
@@ -12,38 +13,10 @@ class OpenAIChatBot
   CONTENT_TYPE = 'application/json'
   LOG_PATH = '/app/logs/open_ai_chatbot_'
 
-  def initialize(redis_host, redis_port, db_host, db_name, db_user, db_password)
-    @redis_host = redis_host
-    @redis_port = redis_port
-    @db_host = db_host
-    @db_name = db_name
-    @db_user = db_user
-    @db_password = db_password
-
-    @logger = initialize_logger
-    sleep(5)
-    @redis_client = initialize_redis
-    log_info("Starting Initialization: #{@pg_client}")
-    log_info("DB HOST: #{db_host} | DB NAME: #{db_name} | DB USER: #{db_user} | DB PASSWORD: #{db_password}")
-    @pg_client = PGClient.new(db_host, db_name, db_user, db_password) rescue nil
-
-    #NOTE: Only run once, then comment out
-    unless @pg_client.table_exists?('embedding_texts')
-      table_name = 'embedding_texts'
-      options_hash = {
-        id: 'serial PRIMARY KEY',
-        content: :string,
-        embeddings_id: :string,
-        created_at: 'timestamp DEFAULT CURRENT_TIMESTAMP'
-      }
-      @pg_client.create_table(table_name, options_hash)
-    end
-
-    log_info("Initalizing database tables")
-    @pg_client.create_table()
-    log_info("Initialized: #{@pg_client}")
+  def initialize
+    @nanny = ServiceNanny.new(:redis, :postgres, :logger, { log_path: LOG_PATH })
   rescue => e
-    handle_error(e)
+    @nanny.handle_error(e)
   end
 
   def run
@@ -51,45 +24,27 @@ class OpenAIChatBot
     listen_to_agents
     process_messages
   rescue => e
-    handle_error(e)
+    @nanny.handle_error(e)
   ensure
     shutdown
   end
 
   private
 
-  def initialize_logger
-    Logger.new(LOG_PATH + timestamp + '.log')
-  end
-
-  def initialize_redis
-    Redis.new(host: @redis_host, port: @redis_port)
-  end
-
   def startup
-    puts 'Starting up ...'
-    log_info("Starting up")
+    @nanny.tell_mother('Starting up ...')
+    @nanny.logger.info("Starting up")
   end
 
   def shutdown
-    log_info("Shutting down")
-  end
-
-  def timestamp
-    Time.now.strftime('%Y%m%d')
-  end
-
-  def handle_error(error)
-    log_error("Error: #{error}")
-    log_error("Backtrace: #{error.backtrace.join("\n")}")
-    puts 'Shit there was an error (see logs)'
-
-    puts 'Restarting process messages.'
-    #process_messages
+    @nanny.tell_mother('Shutting down ...')
+    @nanny.logger.info("Shutting down")
   end
 
   def get_chat(message, api_key = nil)
-    log_info("Received Message: #{message}")
+    @nanny.logger.info("Received Message: #{message}")
+    @nanny.tell_mother("Message Received")
+
     uri = URI(API_ENDPOINT)
 
     key = api_key || ENV['OPENAI_API_KEY']
@@ -98,7 +53,7 @@ class OpenAIChatBot
     response = send_request(uri, request)
     parse_response(response)
   rescue StandardError => e
-    handle_error(e)
+    @nanny.handle_error(e)
   end
 
   def create_request(uri, key, message)
@@ -129,36 +84,31 @@ class OpenAIChatBot
   end
 
   def listen_to_agents
-    puts 'Listening ...'
-    log_info("Listening:")
+    @nanny.tell_mother('Listening ...')
+    @nanny.logger.info("Listening:")
+
     @listening_agents = Thread.new do
       begin
-      log_info("Starting Thread:")
-      @redis_client.subscribe('events') do |on|
-        log_info("Subscribing ...")
+        @nanny.logger.info("Starting Thread:")
+        @nanny.redis.subscribe('events') do |on|
+          @nanny.logger.info("Subscribing ...")
         on.message do |_channel, message|
           begin
-          log_info("Message Recieved: #{message}")
-          puts 'Received message'
-          event = JSON.parse(message)
-          process_event(event) if event['type'] == 'user_input'
+            @nanny.logger.info("Message Recieved: #{message}")
+            @nanny.tell_mother('Received message')
+            event = JSON.parse(message)
+            process_event(event) if event['type'] == 'user_input'
           rescue => e
-            log_info("Error: #{e}")
-            log_info("Backtrace: #{e.backtrace.join("\n")}")
-            raise e
+            @nanny.handle_error(e)
           end
         end
-      end
+        end
       rescue => e
-        log_info("Error: #{e}")
-        log_info("Backtrace: #{e.backtrace.join("\n")}")
-        raise e
+        @nanny.handle_error(e)
       end
     end
   rescue => e
-    log_info("Error: #{e}")
-    log_info("Backtrace: #{e.backtrace.join("\n")}")
-    handle_error(e)
+    @nannt.handle_error(e)
   end
 
   def process_event(event)
@@ -170,16 +120,11 @@ class OpenAIChatBot
   end
 
   def persist_message(message, type)
-    while @pg_client.nil?
-      puts 'Waiting for pg client to be initialized.'
-      sleep 1
-      @pg_client = PGClient.new(@db_host, @db_name, @db_user, @db_password)
-    end
+    id = @nanny.postgres.save_message(message)
+    result = @nanny.redis.publish('events', { type: type, postgres_id: id, message: message}.to_json)
+    @nanny.logger.info("Persisted message: #{message}, Persist result: #{id}, #{result}")
 
-    id = @pg_client.save_message(message)
-    result = @redis_client.publish('events', { type: type, postgres_id: id, message: message}.to_json)
-    log_info("Persisted message: #{message}, Persist result: #{id}, #{result}")
-    puts 'Persisted message.'
+    @nanny.tell_mother 'Persisted message.'
     id
   end
 
@@ -190,36 +135,21 @@ class OpenAIChatBot
   end
 
   def process_message(message)
+    @nanny.logger.info("Processing Message: #{message}")
     response = get_chat(message)
-    puts "Processed Message."
-    log_info("Processed Message: #{message}")
+    @nanny.tell_mother("Processed Message.")
+    @nanny.logger.info("Processed Message: #{message}")
     response
+  rescue => e
+    @nanny.handle_error(e)
   end
 
   def publish_response(response)
-    result = @redis_client.publish('events', { type: :agent_input, agent: 'openai_chat_bot', message: response}.to_json)
-    log_info("Published message: #{response}, Publish result: #{result}")
-    puts 'Published message.'
+    result = @nanny.redis.publish('events', { type: :agent_input, agent: 'openai_chat_bot', message: response}.to_json)
+    @nanny.logger.info("Published message: #{response}, Publish result: #{result}")
+    @nanny.tell_mother('Published message.')
     response
-  end
-
-  def log_info(message)
-    @logger.info("#{self.class.name} - #{message}")
-  end
-
-  def log_error(message)
-    @logger.error("#{self.class.name} - #{message}")
   end
 end
 
-# Example usage
-redis_host = 'redis_container'
-redis_port = 6379
-
-db_host = 'postgres_container'
-db_name = 'postgres'
-db_user = 'postgres'
-db_password = 'postgres'
-
-bot = OpenAIChatBot.new(redis_host, redis_port, db_host, db_name, db_user, db_password)
-bot.run
+OpenAIChatBot.new.run
--- children/openai_chat_bot/service_nanny.rb
+++ children/openai_chat_bot/service_nanny.rb
@@ -1,8 +1,12 @@
+require 'pg'
+
 class ServiceNanny
-  attr_reader :redis, :postgres
+  attr_reader :redis, :postgres, :logger, :options
 
   def initialize(*services)
+    @options = services.last.is_a?(Hash) ? services.pop : {}
     @services = services
+    start
   end
 
   def start
@@ -12,6 +16,12 @@ class ServiceNanny
         @redis = initialize_redis
       when :postgres
         @postgres = initialize_postgres
+
+        initialize_tables
+      when :logger
+        raise 'Missing log_path' if options[:log_path].nil?
+
+        @logger = initialize_logger
       else
         raise "Unknown service: #{service}"
       end
@@ -20,18 +30,49 @@ class ServiceNanny
     self
   end
 
+  def initialize_logger
+    Logger.new(options[:log_path] + timestamp + '.log')
+  end
+
+  def timestamp
+    Time.now.strftime('%Y%m%d')
+  end
+
+  def handle_error(error)
+    raise error unless @logger
+
+    @logger.error("Error: #{error}")
+    @logger.error("Backtrace: #{error.backtrace.join("\n")}")
+
+    raise error
+  end
+
+  def tell_mother(message)
+    puts message
+  end
+
   private
 
   def initialize_redis
-    # Initialize your Redis service here and return the instance
-    # This is just an example, replace with your actual Redis initialization code
     Redis.new(host: 'redis_container', port: 6379)
   end
 
   def initialize_postgres
-    # Initialize your Postgres service here and return the instance
-    # This is just an example, replace with your actual Postgres initialization code
     PGClient.new('postgres_container', 'postgres', 'postgres', 'postgres')
+  end
+
+  def initialize_tables
+    unless @postgres.table_exists?('embedding_texts')
+      table_name = 'embedding_texts'
+      options_hash = {
+        id: 'serial PRIMARY KEY',
+        content: :string,
+        embeddings_id: :string,
+        created_at: 'timestamp DEFAULT CURRENT_TIMESTAMP'
+      }
+
+      @pg_client.create_table(table_name, options_hash)
+    end
   end
 end
 
